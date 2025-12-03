69e8f13b086e249f8373ff3180220632424b98ba
Pham Tien Dat <datgt1103@gmail.com>
2025-12-03 09:41:22 +0700

    Thêm env

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api',
+  production: true,
+  apiUrl: 'https://added-leanora-mailshop-f29e0bf1.koyeb.app/api',
   googleClientId: '558354084862-p0nv0cls220qem34e8mno0a8le2ug1fo.apps.googleusercontent.com'
 };
