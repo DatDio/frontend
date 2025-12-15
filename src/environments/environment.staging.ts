cc6c10046d7c6738ccfdc9d2eddee38d279a3797
Pham Tien Dat <datgt1103@gmail.com>
2025-12-15 10:50:29 +0700

    fix envi

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://backend-zydu.onrender.com/',
+  apiBaseUrl: 'https://backend-zydu.onrender.com',
   apiUrl: 'https://backend-zydu.onrender.com/api',
   wsUrl: 'https://backend-zydu.onrender.com/ws',
   googleClientId: '558354084862-p0nv0cls220qem34e8mno0a8le2ug1fo.apps.googleusercontent.com',
